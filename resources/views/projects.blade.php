31b5b70a133a3aed9d00dc2f0f897a09f526b70f
jarod <jarodiking17@gmail.com>
2021-06-29 21:15:27 +0200

    version 1.3.1
    updated text

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -11,8 +11,17 @@
                         <div class="flex-grow-1">
                             <h3 class="mb-0">The Invaders</h3>
                             <div class="subheading mb-3">C++</div>
-                            <p>The C++ game is a project I made for my exams. It took some time to make, but I'm happy I made it because it gave me a really good score </p>
+                            <p class="mb-0">This C++ game is a project I made for my exams. It took some time to make, but I'm happy I made it because it gave me a really good score </p>
+                            <br>
                             <p class="mb-0">When you start this game you'll see a row of invaders that will shoot at you. It is your job to shoot these invaders and win the day. When you have shot all the invader you'll advance to the next level. Each level you advance, the game will get harder and harder. There will be more aliens and the aliens will shoot more and more. Make sure you dont die and that the aliens dont reach you!</p>
+                            <br>
+                            <p class="mb-0">Requirements</p>
+                            <ul class="mb-0">
+                                <li>Windows 10 or greater</li>
+                                <li>intel i3 or greater</li>
+                                <li>1080p resolution or greater</li>
+                            </ul>
+
                         </div>
                         <div class="flex-shrink-0"><a href="downloadTheInvaders"><i class="fas fa-download"></a></i></div>
                     </div>
