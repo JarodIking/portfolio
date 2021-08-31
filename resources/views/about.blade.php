43f79a8c037d542e4516f2978d0ed1d8e886fbb7
jarod <jarodiking17@gmail.com>
2021-08-31 23:02:19 +0200

    version 1.4.1
    Fixed speilling error

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -14,7 +14,7 @@
                         Utrecht, Amersfoort · +31 653700754 ·
                         <a href="mailto:name@email.com">Jarodiking17@gmail.com</a>
                     </div>
-                    <p class="lead mb-5">I'm a student at ICT Campus MBO 4 software development. I leverage different programming langauges, libraries and frameworks to build applications</p>
+                    <p class="lead mb-5">I'm a student at ICT Campus MBO 4 software development. I leverage different programming languages, libraries and frameworks to build applications</p>
                     <div class="social-icons">
                         <a class="social-icon" href="https://www.linkedin.com/in/jarod-iking-907004162/"><i class="fab fa-linkedin-in"></i></a>
                         <a class="social-icon" href="https://github.com/JarodIking"><i class="fab fa-github"></i></a>
